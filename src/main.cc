071e3b9d8e6025624d3ca85cc4215fa2cc46fa11
Ewan Lemonnier <ewan.lemonnier@epita.fr>
2024-04-04 20:45:36 +0200

    feat(main): Add output, size and help options

--- src/main.cc
+++ src/main.cc
@@ -1,65 +1,72 @@
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <unistd.h>
 
 #include "image2d.hh"
 #include "rendering.hh"
 #include "scene.hh"
 #include "utils.hh"
 
-int main()
+int main(int argc, char* argv[])
 {
-    /*
-    // Image
+    char opt;
 
-    auto aspect_ratio = 4.0 / 4.0;
-    int image_width = 1025; // should be a (power of 2) + 1 for diamond-square
-
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-    Image2D image(image_width, image_height);
-
-    // simplex noise test
-
-    for (int j = 0; j < image_height; j++)
-    {
-        for (int i = 0; i < image_width; i++)
-        {
-            float height = SimplexNoise::noise(i / 100.0, j / 100.0);
-
-            if (height < 0.0f || height > 1.0f)
-            {
-                std::clog << "Error: height out of bounds: " << height
-                          << std::endl;
-                return 1;
-            }
-
-            // std::clog << height << " ";
-            Color pixel_color = Color(0.0, 0.0, 1.0);
-            if (height > 0.3)
-            {
-                pixel_color = Color(0.0, 0.5, 0.0);
-            }
-            if (height > 0.6)
-            {
-                pixel_color = Color(0.5, 0.5, 0.5);
-            }
-            if (height > 0.8)
-            {
-                pixel_color = Color(1.0, 1.0, 1.0);
-            }
-
-            image.setPixel(i, j, pixel_color.r_, pixel_color.g_,
-                           pixel_color.b_);
-        }
-        // std::clog << std::endl;
-    }
-
-    image.writePPM("output.ppm");
-    */
-
+    std::string output_filename = "../images/output.ppm" ;
+    std::string size_str;
     int image_width = 720;
     int image_height = 480;
+    // std::string scene_type;
+    bool show_help = false;
+
+    while ((opt = getopt(argc, argv, "o:s:h")) != -1) {
+        switch (opt) {
+            case 'o':
+                output_filename = optarg;
+                break;
+            case 's':
+                size_str = optarg;
+                break;
+            // case 't':
+            //     scene_type = optarg;
+            //     break;
+            case 'h':
+                show_help = true;
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-o <output_filename>] [-s <width>x<height>] [-h]" << std::endl;
+                return 1;
+        }
+    }
+
+    if (show_help) {
+        std::cout << "Usage: " << argv[0] << " [-o <output_filename>] [-s <width>x<height>] [-h]" << std::endl;
+        std::cout << "Options:" << std::endl;
+        std::cout << "  -o <output_filename>  Specify the path to the output file (default is images/output.ppm)" << std::endl;
+        std::cout << "  -s <width>x<height>   Specify the size of the output image (default is 720x480)" << std::endl;
+        // std::cout << "  -t <scene_type>       Specify the scene type" << std::endl;
+        std::cout << "  -h                    Show this help menu" << std::endl;
+        return 0;
+    }
+
+    if (!size_str.empty()) {
+        size_t pos = size_str.find('x');
+        if (pos != std::string::npos) {
+            std::stringstream ss_width(size_str.substr(0, pos));
+            std::stringstream ss_height(size_str.substr(pos + 1));
+            ss_width >> image_width;
+            ss_height >> image_height;
+            if (image_width <= 0 || image_height <= 0) {
+                std::cerr << "Error: Invalid size format or dimensions. Please use the format '<width>x<height>'." << std::endl;
+                return 1;
+            }
+        } else {
+            std::cerr << "Error: Invalid size format. Please use the format '<width>x<height>'." << std::endl;
+            return 1;
+        }
+    }
+
+    std::clog << "Image size: " << image_width << "x" << image_height << std::endl;
 
     Image2D image(image_width, image_height);
 
@@ -75,7 +82,7 @@ int main()
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "Runtime : " << elapsed.count() << " seconds" << std::endl;
 
-    image.writePPM("../images/output_scene_test.ppm");
+    image.writePPM(output_filename.c_str());
 
     return 0;
 }
