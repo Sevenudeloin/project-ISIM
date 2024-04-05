931d0494d0de3e30cb183298ea341b7dfd29d419
Ewan Lemonnier <ewan.lemonnier@epita.fr>
2024-04-05 13:10:22 +0200

    fix(main options): Change -s to -d and -t to -s

--- src/main.cc
+++ src/main.cc
@@ -23,47 +23,47 @@ int main(int argc, char* argv[])
     char opt;
 
     std::string output_filename = "../images/output.ppm" ;
-    std::string size_str;
+    std::string dim_str;
     int image_width = 720;
     int image_height = 480;
     std::string scene_type = "test";
     bool show_help = false;
 
-    while ((opt = getopt(argc, argv, "o:s:t:h")) != -1) {
+    while ((opt = getopt(argc, argv, "o:d:s:h")) != -1) {
         switch (opt) {
             case 'o':
                 output_filename = optarg;
                 break;
-            case 's':
-                size_str = optarg;
+            case 'd':
+                dim_str = optarg;
                 break;
-            case 't':
+            case 's':
                 scene_type = optarg;
                 break;
             case 'h':
                 show_help = true;
                 break;
             default:
-                std::cerr << "Usage: " << argv[0] << " [-o <output_filename>] [-s <width>x<height>] [-t <scene_type>] [-h]" << std::endl;
+                std::cerr << "Usage: " << argv[0] << " [-o <output_filename>] [-d <width>x<height>] [-s <scene_type>] [-h]" << std::endl;
                 return 1;
         }
     }
 
     if (show_help) {
-        std::cout << "Usage: " << argv[0] << " [-o <output_filename>] [-s <width>x<height>] [-t <scene_type>] [-h]" << std::endl;
+        std::cout << "Usage: " << argv[0] << " [-o <output_filename>] [-d <width>x<height>] [-s <scene_type>] [-h]" << std::endl;
         std::cout << "Options:" << std::endl;
         std::cout << "  -o <output_filename>  Specify the path to the output file (default is images/output.ppm)" << std::endl;
-        std::cout << "  -s <width>x<height>   Specify the size of the output image (default is 720x480)" << std::endl;
-        std::cout << "  -t <scene_type>       Specify the scene type (available: test, ocean, island), (default is test)" << std::endl;
+        std::cout << "  -d <width>x<height>   Specify the dimensions of the output image (default is 720x480)" << std::endl;
+        std::cout << "  -s <scene_type>       Specify the scene (available: test, ocean, island), (default is test)" << std::endl;
         std::cout << "  -h                    Show this help menu" << std::endl;
         return 0;
     }
 
-    if (!size_str.empty()) {
-        size_t pos = size_str.find('x');
+    if (!dim_str.empty()) {
+        size_t pos = dim_str.find('x');
         if (pos != std::string::npos) {
-            std::stringstream ss_width(size_str.substr(0, pos));
-            std::stringstream ss_height(size_str.substr(pos + 1));
+            std::stringstream ss_width(dim_str.substr(0, pos));
+            std::stringstream ss_height(dim_str.substr(pos + 1));
             ss_width >> image_width;
             ss_height >> image_height;
             if (image_width <= 0 || image_height <= 0) {
