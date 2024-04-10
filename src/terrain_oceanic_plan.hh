ac8d0341103b76724bd2161b35e0b8c40b9d6bdf
oscarmorand <oscar.morand@epita.fr>
2024-04-10 17:31:00 +0200

    fear(oceanic plan)

--- src/terrain_oceanic_plan.hh
+++ src/terrain_oceanic_plan.hh
@@ -9,13 +9,14 @@ public:
     double height_;
     Vector3 n_;
 
-    TerrainOceanicPlan(double height)
-    {}
+    TerrainOceanicPlan(double height, std::shared_ptr<TerrainLayerTexture> mat);
 
     bool hit(const Ray &ray, HitRecord &hit_record) const override;
 
     void translate(const Vector3 &v) override;
 
+    Point3 get_uv(const Point3 &p) const;
+
     LocalTexture get_texture_at(const Point3 &p) const override;
     Vector3 get_normal_at(const Point3 &p) const override;
-}
+};
